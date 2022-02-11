2b4ce0fb4f91e3b63d30d13bf23c0da05ece0948
Lukáš Tinkl <lukas@kde.org>
2022-02-11 02:12:39 +0100

    update l10n

--- i18n/base.ts
+++ i18n/base.ts
@@ -9,34 +9,44 @@
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../main.qml" line="132"/>
+        <location filename="../main.qml" line="25"/>
+        <source>Game number %1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../main.qml" line="34"/>
+        <source>Restart Game</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../main.qml" line="157"/>
         <source>Game Won!</source>
         <translation type="unfinished"></translation>
     </message>
     <message numerus="yes">
-        <location filename="../main.qml" line="133"/>
+        <location filename="../main.qml" line="158"/>
         <source>Congratulations&lt;br&gt;&lt;br&gt;You won the game in %n turn(s)</source>
         <translation type="unfinished">
             <numerusform></numerusform>
         </translation>
     </message>
     <message>
-        <location filename="../main.qml" line="145"/>
+        <location filename="../main.qml" line="170"/>
         <source>Game Lost :(</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../main.qml" line="146"/>
-        <source>Unfortunately you couldn&apos;t make it this time</source>
+        <location filename="../main.qml" line="171"/>
+        <source>Unfortunately you couldn&apos;t make it this time&lt;br&gt;The word was: &apos;%1&apos;</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../main.qml" line="152"/>
+        <location filename="../main.qml" line="178"/>
         <source>Word Not Found</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../main.qml" line="153"/>
+        <location filename="../main.qml" line="179"/>
         <source>The word &apos;%1&apos; was not found in dictionary, try again.</source>
         <translation type="unfinished"></translation>
     </message>
--- i18n/qml_cs.ts
+++ i18n/qml_cs.ts
@@ -9,36 +9,50 @@
         <translation>Wurdl</translation>
     </message>
     <message>
-        <location filename="../main.qml" line="132"/>
+        <location filename="../main.qml" line="25"/>
+        <source>Game number %1</source>
+        <translation>Hra číslo %1</translation>
+    </message>
+    <message>
+        <location filename="../main.qml" line="34"/>
+        <source>Restart Game</source>
+        <translation>Restartovat hru</translation>
+    </message>
+    <message>
+        <location filename="../main.qml" line="157"/>
         <source>Game Won!</source>
         <translation>Vítězství!</translation>
     </message>
     <message numerus="yes">
-        <location filename="../main.qml" line="133"/>
+        <location filename="../main.qml" line="158"/>
         <source>Congratulations&lt;br&gt;&lt;br&gt;You won the game in %n turn(s)</source>
-        <translation>
+        <translation type="unfinished">
             <numerusform>Gratulujeme&lt;br&gt;&lt;br&gt;Vyhráli jste hru v %n tahu</numerusform>
-            <numerusform>Gratulujeme&lt;br&gt;&lt;br&gt;Vyhráli jste hru v %n tazích</numerusform>
+            <numerusform>Gratulujeme&lt;br&gt;&lt;br&gt;Vyhráli jste hru ve %n tazích</numerusform>
             <numerusform>Gratulujeme&lt;br&gt;&lt;br&gt;Vyhráli jste hru v %n tazích</numerusform>
         </translation>
     </message>
     <message>
-        <location filename="../main.qml" line="145"/>
+        <location filename="../main.qml" line="170"/>
         <source>Game Lost :(</source>
         <translation>Prohra :(</translation>
     </message>
     <message>
-        <location filename="../main.qml" line="146"/>
-        <source>Unfortunately you couldn&apos;t make it this time</source>
-        <translation>Bohužel to tentokrát nevyšlo</translation>
+        <location filename="../main.qml" line="171"/>
+        <source>Unfortunately you couldn&apos;t make it this time&lt;br&gt;The word was: &apos;%1&apos;</source>
+        <translation>Bohužel to tentokrát nevyšlo&lt;br&gt;Dané slovo bylo: %1</translation>
     </message>
     <message>
-        <location filename="../main.qml" line="152"/>
+        <source>Unfortunately you couldn&apos;t make it this time</source>
+        <translation type="vanished">Bohužel to tentokrát nevyšlo</translation>
+    </message>
+    <message>
+        <location filename="../main.qml" line="178"/>
         <source>Word Not Found</source>
         <translation>Slovo nenalezeno</translation>
     </message>
     <message>
-        <location filename="../main.qml" line="153"/>
+        <location filename="../main.qml" line="179"/>
         <source>The word &apos;%1&apos; was not found in dictionary, try again.</source>
         <translation>Slovo &apos;%1&apos; nebylo nalezeno ve slovníku, zkuste to znovu.</translation>
     </message>
