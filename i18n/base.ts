d73260a2d038861a79c785989f75e6a7e7208d2e
Lukáš Tinkl <lukas@kde.org>
2022-02-13 00:59:16 +0100

    update l10n

--- i18n/base.ts
+++ i18n/base.ts
@@ -9,64 +9,79 @@
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../main.qml" line="27"/>
+        <location filename="../main.qml" line="25"/>
         <source>Menu</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../main.qml" line="36"/>
+        <location filename="../main.qml" line="34"/>
         <source>Random Game</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../main.qml" line="41"/>
+        <location filename="../main.qml" line="39"/>
         <source>Previous Games...</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../main.qml" line="47"/>
+        <location filename="../main.qml" line="45"/>
         <source>Dark Mode</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../main.qml" line="59"/>
+        <location filename="../main.qml" line="57"/>
         <source>Game number %1</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../main.qml" line="67"/>
+        <location filename="../main.qml" line="64"/>
         <source>Restart Game</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../main.qml" line="196"/>
+        <location filename="../main.qml" line="94"/>
+        <source>Play Previous Game</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../main.qml" line="100"/>
+        <source>Select from one of the previous games:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../main.qml" line="112"/>
+        <source>(counting started on 2021-02-02)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../main.qml" line="222"/>
         <source>Game Won!</source>
         <translation type="unfinished"></translation>
     </message>
     <message numerus="yes">
-        <location filename="../main.qml" line="197"/>
+        <location filename="../main.qml" line="223"/>
         <source>Congratulations&lt;br&gt;&lt;br&gt;You won the game in %n turn(s)</source>
         <translation type="unfinished">
             <numerusform></numerusform>
         </translation>
     </message>
     <message>
-        <location filename="../main.qml" line="209"/>
+        <location filename="../main.qml" line="235"/>
         <source>Game Lost :(</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../main.qml" line="210"/>
+        <location filename="../main.qml" line="236"/>
         <source>Unfortunately you couldn&apos;t make it this time&lt;br&gt;The word was: &apos;%1&apos;</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../main.qml" line="217"/>
+        <location filename="../main.qml" line="243"/>
         <source>Word Not Found</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../main.qml" line="218"/>
+        <location filename="../main.qml" line="244"/>
         <source>The word &apos;%1&apos; was not found in dictionary, try again.</source>
         <translation type="unfinished"></translation>
     </message>
--- i18n/qml_cs.ts
+++ i18n/qml_cs.ts
@@ -9,42 +9,57 @@
         <translation>Wurdl</translation>
     </message>
     <message>
-        <location filename="../main.qml" line="27"/>
+        <location filename="../main.qml" line="25"/>
         <source>Menu</source>
         <translation>Menu</translation>
     </message>
     <message>
-        <location filename="../main.qml" line="36"/>
+        <location filename="../main.qml" line="34"/>
         <source>Random Game</source>
         <translation>Náhodná hra</translation>
     </message>
     <message>
-        <location filename="../main.qml" line="41"/>
+        <location filename="../main.qml" line="39"/>
         <source>Previous Games...</source>
         <translation>Předchozí hry...</translation>
     </message>
     <message>
-        <location filename="../main.qml" line="47"/>
+        <location filename="../main.qml" line="45"/>
         <source>Dark Mode</source>
         <translation>Tmavý režim</translation>
     </message>
     <message>
-        <location filename="../main.qml" line="59"/>
+        <location filename="../main.qml" line="57"/>
         <source>Game number %1</source>
         <translation>Hra číslo %1</translation>
     </message>
     <message>
-        <location filename="../main.qml" line="67"/>
+        <location filename="../main.qml" line="64"/>
         <source>Restart Game</source>
         <translation>Restartovat hru</translation>
     </message>
     <message>
-        <location filename="../main.qml" line="196"/>
+        <location filename="../main.qml" line="94"/>
+        <source>Play Previous Game</source>
+        <translation type="unfinished">Hrát předchozí hru</translation>
+    </message>
+    <message>
+        <location filename="../main.qml" line="100"/>
+        <source>Select from one of the previous games:</source>
+        <translation>Zvolte jednu z předchocích her:</translation>
+    </message>
+    <message>
+        <location filename="../main.qml" line="112"/>
+        <source>(counting started on 2021-02-02)</source>
+        <translation>(počítání začalo 2.2. 2021)</translation>
+    </message>
+    <message>
+        <location filename="../main.qml" line="222"/>
         <source>Game Won!</source>
         <translation>Vítězství!</translation>
     </message>
     <message numerus="yes">
-        <location filename="../main.qml" line="197"/>
+        <location filename="../main.qml" line="223"/>
         <source>Congratulations&lt;br&gt;&lt;br&gt;You won the game in %n turn(s)</source>
         <translation>
             <numerusform>Gratulujeme&lt;br&gt;&lt;br&gt;Vyhráli jste hru v %n tahu</numerusform>
@@ -53,12 +68,12 @@
         </translation>
     </message>
     <message>
-        <location filename="../main.qml" line="209"/>
+        <location filename="../main.qml" line="235"/>
         <source>Game Lost :(</source>
         <translation>Prohra :(</translation>
     </message>
     <message>
-        <location filename="../main.qml" line="210"/>
+        <location filename="../main.qml" line="236"/>
         <source>Unfortunately you couldn&apos;t make it this time&lt;br&gt;The word was: &apos;%1&apos;</source>
         <translation>Bohužel to tentokrát nevyšlo&lt;br&gt;Dané slovo bylo: %1</translation>
     </message>
@@ -67,12 +82,12 @@
         <translation type="vanished">Bohužel to tentokrát nevyšlo</translation>
     </message>
     <message>
-        <location filename="../main.qml" line="217"/>
+        <location filename="../main.qml" line="243"/>
         <source>Word Not Found</source>
         <translation>Slovo nenalezeno</translation>
     </message>
     <message>
-        <location filename="../main.qml" line="218"/>
+        <location filename="../main.qml" line="244"/>
         <source>The word &apos;%1&apos; was not found in dictionary, try again.</source>
         <translation>Slovo &apos;%1&apos; nebylo nalezeno ve slovníku, zkuste to znovu.</translation>
     </message>
